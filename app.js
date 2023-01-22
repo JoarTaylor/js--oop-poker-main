ef76ca6df0634ebdd0c127baa5fe1ddc8675ae02
JoarTaylor <joar.taylor@outlook.com>
2023-01-22 18:52:02 +0100

    edit

--- app.js
+++ app.js
@@ -83,7 +83,7 @@ class cardDeck extends Player{
         
         suits.forEach( (suit) => {
             values.forEach((value) => {
-                this.deck.push({'Name': `${suit} of ${value.Name}`, 'Value': value.Value});
+                this.deck.push({'Name': `${suit} of ${value.Name}`, 'Suit': suit, 'Value': value.Value});
             })
         })
     }
@@ -204,7 +204,7 @@ class Validate{
     constructor() {
     }
 
-    static calculate(people) {
+    static calculate(people, numPlayers) {
         const rankArr = people.sort((a, b) => {
             if((a.handValue() < b.handValue())) {
               return 1;
@@ -222,10 +222,41 @@ class Validate{
                 }
          )
         
-        //validate hands like in real poker
-         people.forEach(person => {
+         
+        //validate hands like in real poker.....
+         for (let i = 0; i < numPlayers; i++) {
+            const arr = []
+            people[i].displayHand().forEach(person => {
+                arr.push(person.Value)
+            })
+            console.log(arr)
+            var a = arr.reduce(function (acc, curr) {
+                if (typeof acc[curr] == 'undefined') {
+                  acc[curr] = 1;
+                } else {
+                  acc[curr] += 1;
+                }
+              
+                return acc;
+              }, {});
+              console.log(a)
+              const myEntries = Object.entries(a);
+              const maxArr = [];
+              myEntries.forEach(([key, value]) => {
+                maxArr.push(value);
+              })
+              const highestCombo = Math.max(...maxArr);
 
-         })
+              console.log('Name: ', people[i].playerName)
+
+              switch (highestCombo) {
+                case 1: console.log('high hand'); break;
+                case 2: console.log('one pair'); break;
+                case 3: console.log('three of a kind'); break;
+                case 4: console.log('quad'); break;
+              }
+         }
+
 
         return handsByValue;
     }
@@ -267,7 +298,7 @@ class Game {
                 this.playersList[i].throwCard(cardIndexesToSwap, myDeck)
                 this.playersList[i].deal(cardIndexesToSwap.length, myDeck)
             }
-            Validate.calculate(this.playersList);
+            Validate.calculate(this.playersList, numPlayers);
         }
     }
 
